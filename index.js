b2389f1f9620ac730b5df0a9a41363225f28b2f8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2020-04-14 21:51:21 -0500

    Add more reviews

--- index.js
+++ index.js
@@ -245,8 +245,8 @@ async function attachReviewInfoToStories(stories) {
       story.reviews = [];
     }
 
-    story.codeReviews = story.reviews.filter(review => review.review_type_id === 7604 || review.review_type_id === 4628937 || review.review_type_id === 4165914 || review.review_type_id === 618);
-    story.qaReviews = story.reviews.filter(review => review.review_type_id === 7602 || review.review_type_id === 4628939 || review.review_type_id === 4165912 || review.review_type_id === 616);
+    story.codeReviews = story.reviews.filter(review => review.review_type_id === 7604 || review.review_type_id === 4628937 || review.review_type_id === 4165914 || review.review_type_id === 618 || review.review_type_id === 342 || review.review_type_id === 5340178);
+    story.qaReviews = story.reviews.filter(review => review.review_type_id === 7602 || review.review_type_id === 4628939 || review.review_type_id === 4165912 || review.review_type_id === 616 || review.review_type_id === 340);
     story.designReviews = story.reviews.filter(review => review.review_type_id === 7603 || review.review_type_id === 617);
     story.featureFlagReviews = story.reviews.filter(review => review.review_type_id === 5527847 || review.review_type_id === 5675322);
 
